668b6c9a0faaa2bdca3ead655e8c6ec874eb76bc
seipan <yamasakipann0218@gmail.com>
2023-09-07 01:13:16 +0900

    ""

---
+++ git.sh
@@ -0,0 +1,13 @@
+# 現在のブランチを取得する
+branch = git branch --contains | cut -d " " -f 2
+
+# pull
+git pull origin $branch
+
+#全部ステージングする
+git add .
+#コミットする
+git commit -m \"$1\"
+
+#プッシュする
+git push origin $branch
